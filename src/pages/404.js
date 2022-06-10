49b5e75760c58e78e4023f6d7fd55654d46d6953
polinnab <p.tsunenko94@gmail.com>
2022-06-10 13:27:07 +0300

    404 and static.json

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,20 +4,31 @@ import Seo from "../components/seo"
 
 const NotFoundPage = (props) => {
   const [customHTML, setCustomHTML] = useState(null)
+  const [blob, setBlob] = useState(null)
   const [loader, setLoader] = useState(false)
   const { pathname } = useLocation()
   console.log('pathname: ', pathname)
 
   function renderCustomPage(path) {
     setLoader(true)
-    fetch(`https://anadea.info/${path}`)
+    fetch(`https://anadea.info`)
     .then(function(response) {
-      debugger
+      const headers = [...response.headers]
+      const index = headers.findIndex(el => el[0] === 'content-type' && el[1] === 'image/jpeg')
+      if (index === -1) {
         return response.text()
+      } else {
+        return response.blob()
+      }
     })
-    .then(function(html) {
-      debugger
-        setCustomHTML(html)
+    .then(function(resp) {
+      if (typeof(resp) === 'string') {
+        setCustomHTML(resp)
+        return
+      } 
+      var objectURL = URL.createObjectURL(resp)
+      console.log(objectURL)
+      setBlob(objectURL)
     })
     .catch(function(err) {  
         console.log('Failed to fetch page: ', err);  
@@ -25,20 +36,23 @@ const NotFoundPage = (props) => {
     setLoader(false)
   }
 
-  // blob
-
-  //render image without <img />
-
   useEffect(() => {
     renderCustomPage('')
   }, [])
+
+  if (blob) {
+    return(
+      <>
+        <img src={blob} alt="" />
+      </>
+    )
+  }
 
   return(
     <>
       <Seo title="404: Not found" />
       {loader && <div>Loading...</div>}
-      <div dangerouslySetInnerHTML={{__html: customHTML}}>{customHTML}</div>
-
+      <div dangerouslySetInnerHTML={{__html: customHTML}}></div>
     </>
   )
 }
