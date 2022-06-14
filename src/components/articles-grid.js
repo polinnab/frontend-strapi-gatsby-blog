e3ab03ee7843c496af7a465e4e0d2ba1fe41d599
polinnab <p.tsunenko94@gmail.com>
2022-06-14 13:17:00 +0300

    keys and static folder

--- src/components/articles-grid.js
+++ src/components/articles-grid.js
@@ -5,7 +5,7 @@ const ArticlesGrid = ({ articles }) => {
   return (
     <div className="container mt-12 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
       {articles.map((article) => (
-        <ArticleCard article={article} />
+        <ArticleCard key={article.slug} article={article} />
       ))}
     </div>
   )
